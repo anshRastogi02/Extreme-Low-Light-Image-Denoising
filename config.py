48a224c4b60a3d192e43e7c3c79a64bd9e800bdf
anshRastogi02 <rastogiansh02@gmail.com>
2024-06-18 18:44:19 +0530

    Editing main file

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+os.environ["KERAS_BACKEND"] = "tensorflow"
+import tensorflow as tf
+from tensorflow import keras
 import random
 SEED = 10
 random.seed(SEED)
 tf.random.set_seed(SEED)
-
+pixel=7
 IMAGE_SIZE = 256
 MAX_TRAIN_IMAGES = 400
 BATCH_SIZE = 16
@@ -13,10 +16,7 @@ TEST_IMAGE_DIR = "lol_dataset/test/low"
 LEARNING_RATE = 1e-4
 LOG_INTERVALS = 10
 EPOCHS = 60
-import os
-os.environ["KERAS_BACKEND"] = "tensorflow"
-import tensorflow as tf
-from tensorflow import keras
+
 
 # print(tf.version.VERSION)
 # AUTOTUNE = tf.data.AUTOTUNE
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
+from config import pixel
 from Model.ZeroDCE import ZeroDCE
 from PSNR import psnr
 
@@ -12,12 +13,14 @@ from PSNR import psnr
 
 # Whole dataset: training + testing = 500 images
 # Update the path of training low exposured an enhanced images
-low_image_files = sorted(glob(os.path.join(r"C:\Denoising\Extreme-Low-Light-Image-Denoising\dataset\test\low", "*.png")) + glob(os.path.join(r"C:\Denoising\Extreme-Low-Light-Image-Denoising\dataset\train\low", "*.png")))
+# low_image_files = sorted(glob(os.path.join(r"C:\Denoising\Extreme-Low-Light-Image-Denoising\dataset\test\low", "*.png")) + glob(os.path.join(r"C:\Denoising\Extreme-Low-Light-Image-Denoising\dataset\train\low", "*.png")))
+low_image_files = sorted(glob(os.path.join(r"C:\Denoising\Extreme-Low-Light-Image-Denoising\dataset\test\low", "*.png")))
 
 # Loading Saved Models
 # print("---------------------------------------------------------------")
 print("Choose the weight_file to load: ")
 print(os.listdir("saved_weights"))
+
 wt = input("Enter the model in xyz.h5 format: ")
 new_zero_dce_model = ZeroDCE()
 new_zero_dce_model.load_weights(f"saved_weights/{wt}")
@@ -41,27 +44,26 @@ def progress_bar(progress, total):
     print(f"\r|{bar}| {percent:.2f}%", end="\r")
 
 
-
 psnr_array=[]
 it = 1
 progress_bar(0,len(low_image_files))
 
-print("")
-print(f"Infering {len(low_image_files)} images using {wt} weights")
 
 enhanced_image_files=[]
-
 # Infering images of whole dataset
 for original_image in (low_image_files):
-    original_image = Image.open(original_image)
+    original_image = Image.open(original_image).convert('RGB')
     output_image = infer(original_image)
     enhanced_image_files.append(output_image)
-    original_image = np.array(original_image).astype(np.float32)/255.0
-    output_image = np.array(output_image).astype(np.float32)/255.0
+    original_image = np.array(original_image).astype(np.float32)/float(pixel)
+    output_image = np.array(output_image).astype(np.float32)/float(pixel)
     val =psnr(original_image,output_image)
     psnr_array.append(val)
     it+=1    
     progress_bar(it, len(low_image_files))
+
+print("")
+print(f"Infering {len(low_image_files)} images using {wt} weights")
 
 
 # Plot Original vs Enhanced Images
@@ -79,7 +81,6 @@ def plot_results(images, pred_images,psnr=0,figure_size=(15,15)):
     plt.suptitle(f"Average PSNR = {float(psnr):.4f}").set_color('b')
     fig.tight_layout()   
     plt.show() 
-
 
 print(f"Average PSNR value of {len(low_image_files)+len(enhanced_image_files)} files is : {float(np.mean(psnr_array)):.4f}")
 
